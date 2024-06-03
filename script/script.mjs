e6b68cd942da334c4a008bc336bc0d39927118f7
ebelbell <ebella2056@gmail.com>
2024-06-03 04:22:07 -0700

    added code to fetch data from the API

--- script/script.mjs
+++ script/script.mjs
@@ -9,21 +9,26 @@ Organize files using modules and imports.
 
 
 async function fetchData() {
-    // Two lines. Fetches data. All we need in most cases
-    const result = await fetch("https://api.disneyapi.dev/character");
+    // Two lines. Fetches data. The "await" means to wait for the data to be fetched
+    const result = await fetch("https://api.disneyapi.dev/character"); //fetch the server and go to the list of disney characters
+    //console.log(result);
+    
     const data = await result.json();
+    console.log(data);
+
   
     // Loops through the list of pokemon fetched from the api
-    for (let i = 0; i < data.results.length; i++) {
-      // create a new h1 element
-      const nameDisplay = document.createElement("h1");
+  for (let i = 0; i < data.results.length; i++) {
+    // create a new h1 element
+    const nameDisplay = document.querySelector('h1'); //select h1 and assign it to something 
   
-      // sets the text of the element to pokemons name
-      nameDisplay.innerText = data.results[i].name;
-  
-      // displays the element by appending it to the body
-      document.querySelector("body").appendChild(nameDisplay);
-    }
+    // sets the text of the element to the disney characters' name
+    nameDisplay.innerText = data.results[i].name;
+
+    // displays the element by appending it to the body
+    document.querySelector("body").appendChild(nameDisplay);
+
   }
+}
   
   fetchData();
