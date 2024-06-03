808a1b8a90a37b4cfc9e3b7ad26d1669ff2eedd6
ebelbell <ebella2056@gmail.com>
2024-06-03 05:05:36 -0700

    Update script.mjs

--- script/script.mjs
+++ script/script.mjs
@@ -9,26 +9,33 @@ Organize files using modules and imports.
 
 
 async function fetchData() {
-    // Two lines. Fetches data. The "await" means to wait for the data to be fetched
-    const result = await fetch("https://api.disneyapi.dev/character"); //fetch the server and go to the list of disney characters
-    //console.log(result);
-    
-    const data = await result.json();
-    console.log(data);
+    // try to fetch the data
+    try {
+        // Two lines. Fetches data. The "await" means to wait for the data to be fetched
+        const results = await fetch("https://api.disneyapi.dev/character"); //fetch the server and go to the list of disney characters
+        if(!results.valid) {
+        // console.log(result);
+        throw new Error("Network response was not valid");
+        } 
 
-  
-    // Loops through the list of pokemon fetched from the api
-  for (let i = 0; i < data.results.length; i++) {
-    // create a new h1 element
-    const nameDisplay = document.querySelector('h1'); //select h1 and assign it to something 
-  
-    // sets the text of the element to the disney characters' name
-    nameDisplay.innerText = data.results[i].name;
+        const data = await results.json();
+        console.log(data);
+        
+        
+        // Loops through the list of pokemon fetched from the api
+        for (let i = 0; i < data.results.length; i++) {
+            // create a new h1 element
+            const nameDisplay = document.createElement('h1'); //select h1 and assign it to something 
+        
+            // sets the text of the element to the disney characters' name
+            nameDisplay.innerText = data.results[i].name;
 
-    // displays the element by appending it to the body
-    document.querySelector("body").appendChild(nameDisplay);
-
-  }
+            // displays the element by appending it to the body
+            document.querySelector("body").appendChild(nameDisplay);
+        }
+    } catch (err) {
+        console.log("Fetch error: ", err);
+    }
 }
   
-  fetchData();
+fetchData();
