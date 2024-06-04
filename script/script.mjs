181456a4155362676afa627c3d872a33b339751a
ebelbell <ebella2056@gmail.com>
2024-06-04 16:59:37 -0700

    Update script.mjs
    switched to Avatar the Last Airbender API

--- script/script.mjs
+++ script/script.mjs
@@ -13,45 +13,41 @@ function delay(ms) {
 }
 
 async function fetchData() {
-    let page = 1;
-    let totalPages = 1;
-
     //try to fetch the data
     try {
 
-        //id that the user types in
-        const herculesSprite = document.getElementById("herculesSprite").value.toLowerCase(); //the .toLowerCase() method makes it so that if the user types in caps, the value will be set to all lowercase for easier access
-
+        //id that the user types in, get the elementName
+        const disneyFilm = document.getElementById("disneyFilm").value.toLowerCase(); //the .toLowerCase() method makes it a chain method so that if the user types in caps, the value will be set to all lowercase for easier access
         //two lines. Fetches data. The "await" means to wait for the data to be fetched
-        const results = await fetch(`https://api.disneyapi.dev/character/${herculesSprite}`); //fetch the server and go to the list of disney characters
+        const results = await fetch("https://last-airbender-api.fly.dev/api/v1/characters"); //fetch the server and go to the list of disney characters
+        console.log(results);
 
-            if(results.ok) {
-                console.log('Success!')
-            } else {
+            if(!results.ok) {
                 throw new Error("Network response was not valid");
-            } 
+            }     
 
             const data = await results.json();
             console.log(data);
-
-            //loops through the list of Disney characters fetched from the api
-            for (let i = 0; i < data.data.length; i++) {
-                //wait 1 seconds before displaying each character's name
-                await delay(1000);
-
-                //create a new h1 element
-                const nameDisplay = document.createElement("h1"); //select h1 and assign it to something 
-            
-                //sets the text of the element to the disney movies' name
-                nameDisplay.innerText = data.data[i].films;
-
-                //displays the element by appendingit g it to the body
-                document.querySelector("body").appendChild(nameDisplay);
-            }
-
-    } catch (err) {
+    }
+    catch (err) {
         console.log("Fetch error: ", err);
     }
+
+            // //loops through the list of Disney characters fetched from the api
+            // for (let i = 0; i < data.data.length; i++) {
+            //     //wait 1 seconds before displaying each character's name
+            //     await delay(1000);
+
+            //     //create a new h1 element
+            //     const nameDisplay = document.createElement("h1"); //select h1 and assign it to something 
+            
+            //     //sets the text of the element to the disney movies' name
+            //     nameDisplay.innerText = data.data[i].films;
+
+            //     //displays the element by appendingit g it to the body
+            //     document.querySelector("body").appendChild(nameDisplay);
+            // }
+ 
 }
 
 //fetchData callback function
