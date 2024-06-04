09397c2de2b75d3537de916a4e1a5a75ddec801c
ebelbell <ebella2056@gmail.com>
2024-06-04 15:33:24 -0700

    Update script.mjs
    created an id for the disney movie entry

--- script/script.mjs
+++ script/script.mjs
@@ -7,44 +7,52 @@ Use fetch and/or Axios to interact with an external web API.
 Organize files using modules and imports.
 */ 
 
+//create a delay for each character name
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function fetchData() {
-    // try to fetch the data
+    let page = 1;
+    let totalPages = 1;
+
+    //try to fetch the data
     try {
-        // Two lines. Fetches data. The "await" means to wait for the data to be fetched
-        const results = await fetch("https://api.disneyapi.dev/character"); //fetch the server and go to the list of disney characters
 
-        if(!results.ok) {
-        // console.log(result);
-        throw new Error("Network response was not valid");
-    } 
+        //id that the user types in
+        const herculesSprite = document.getElementById("herculesSprite").value.toLowerCase(); //the .toLowerCase() method makes it so that if the user types in caps, the value will be set to all lowercase for easier access
 
-        const data = await results.json();
-        console.log(data);
+        //two lines. Fetches data. The "await" means to wait for the data to be fetched
+        const results = await fetch(`https://api.disneyapi.dev/character/${herculesSprite}`); //fetch the server and go to the list of disney characters
 
-        // create a delay for each character name
-        function delay(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-        
+            if(results.ok) {
+                console.log('Success!')
+            } else {
+                throw new Error("Network response was not valid");
+            } 
 
-        // Loops through the list of Disney characters fetched from the api
-        for (let i = 0; i < data.data.length; i++) {
-            // wait 2 seconds before displaying each character's name
-            await delay(1000);
+            const data = await results.json();
+            console.log(data);
 
-            // create a new h1 element
-            const nameDisplay = document.createElement("h1"); //select h1 and assign it to something 
-        
-            // sets the text of the element to the disney characters' name
-            nameDisplay.innerText = data.data[i].name;
+            //loops through the list of Disney characters fetched from the api
+            for (let i = 0; i < data.data.length; i++) {
+                //wait 1 seconds before displaying each character's name
+                await delay(1000);
 
-            // displays the element by appending it to the body
-            document.querySelector("body").appendChild(nameDisplay);
-        }
+                //create a new h1 element
+                const nameDisplay = document.createElement("h1"); //select h1 and assign it to something 
+            
+                //sets the text of the element to the disney movies' name
+                nameDisplay.innerText = data.data[i].films;
+
+                //displays the element by appendingit g it to the body
+                document.querySelector("body").appendChild(nameDisplay);
+            }
+
     } catch (err) {
         console.log("Fetch error: ", err);
     }
 }
-  
+
+//fetchData callback function
 fetchData();
